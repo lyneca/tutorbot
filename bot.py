d1b96a2e0f2bd98ae1d9f0152be10eb527ff85a0
Lyneca <atraguliauniliantauthr@gmail.com>
2018-12-12 10:27:41 +1100

    Add Slack Challenge response

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/events', methods=['POST'])
 def mention():
     """ Called when bot has received a Slack event """
-    ...
+    req_json = request.get_json()
+    if 'challenge' in req_json:
+        return req_json['challenge']
 
 @app.route('/command', methods=['POST'])
 def command():
