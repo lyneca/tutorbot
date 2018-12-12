6bce209f4a533516900c3d586e79102f737e3160
Lyneca <atraguliauniliantauthr@gmail.com>
2018-12-12 13:24:56 +1100

    Update server to new bot code

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
 from flask import Flask, request
+from tutorbot import Bot
 
 app = Flask(__name__)
 
+bot = Bot()
+
+
 @app.route('/events', methods=['POST'])
 def mention():
-    """ Called when bot has received a Slack event """
+    """Called when bot has received a Slack event"""
     req_json = request.get_json()
     if 'challenge' in req_json:
         return req_json['challenge']
 
+
 @app.route('/command', methods=['POST'])
 def command():
-    """ Called when the bot is activated via Slash command """
+    """Called when the bot is activated via Slash command"""
     ...
